1081269af8ba28fa91f73b89fbcb9c4bf1daea32
zNiffler <121610450+zNiffler@users.noreply.github.com>
2023-04-07 16:37:03 +0400

    Add files via upload

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+from datetime import datetime
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-date_in'
+    template_name = 'news.html'
+    context_object_name = 'post_news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'onenews.html'
+    context_object_name = 'onenews'
